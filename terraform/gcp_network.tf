f2563fe51cdd5d1280557fac030d08acbee055a3
Andres Castano <andres@3wks.com.au>
2019-06-03 14:27:54 +1000

    Added velostrata subnet to gcp

--- terraform/gcp_network.tf
+++ terraform/gcp_network.tf
@@ -16,6 +16,15 @@ resource "google_compute_subnetwork" "private-subnet" {
   enable_flow_logs         = true
 }
 
+resource "google_compute_subnetwork" "velostrata-subnet" {
+  name                     = "velostrata-subnet"
+  ip_cidr_range            = "${var.gcp_velostrata_cidr}"
+  project                  = "${var.gcp_projectId}"
+  region                   = "${var.gcp_region}"
+  network                  = "${google_compute_network.demo-vpc.self_link}"
+  enable_flow_logs         = true
+}
+
 resource "google_compute_network" "demo-vpc" {
   name                    = "demo-vpc"
   auto_create_subnetworks = false
