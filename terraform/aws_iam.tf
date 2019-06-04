a88d18f7f2722a1e1e704bbf77988763b318e3c6
Andres Castano <andres@3wks.com.au>
2019-06-04 18:50:52 +1000

    Add velostrata iam group and user

--- terraform/aws_iam.tf
+++ terraform/aws_iam.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudformation_stack" "velostrata-iam" {
+resource "aws_cloudformation_stack" "velostrata-iam-group" {
     name = "velostrata-iam"
     
     capabilities = ["CAPABILITY_IAM"]
@@ -10,4 +10,22 @@ resource "aws_cloudformation_stack" "velostrata-iam" {
     template_body = <<STACK
     ${file("aws_cloudconfig/velostrata_cloudformation.json")}
     STACK
+}
+
+resource "aws_iam_user" "velostrata-iam-user" {
+  name = "velostrata-user"
+}
+
+resource "aws_iam_access_key" "velostrata-iam-key" {
+  user    = "${aws_iam_user.velostrata-iam-user.name}"
+}
+
+resource "aws_iam_group_membership" "velostrata-iam-membership" {
+  name = "velostrata-iam-membership"
+
+  users = [
+    "${aws_iam_user.velostrata-iam-user.name}"
+  ]
+
+  group = "${aws_cloudformation_stack.velostrata-iam-group.outputs["iamGroup"]}"
 }
--- terraform/aws_output.tf
+++ terraform/aws_output.tf
@@ -9,3 +9,11 @@ output "AWS Bastion Elastic DNS name" {
 output "AWS wp-server private ips" {
   value = "${zipmap(aws_instance.web-server.*.id, aws_instance.web-server.*.private_ip)}"
 }
+
+output "Velostrata user name" {
+  value = "${aws_iam_access_key.velostrata-iam-key.user}"
+}
+
+output "Velostrata user secret" {
+  value = "${aws_iam_access_key.velostrata-iam-key.secret}"
+}
